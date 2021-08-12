843742bd1740d6f09d1ceb8846a7f37352a2e255
Amshul-korlepara <82725729+Amshul-korlepara@users.noreply.github.com>
2021-08-12 14:44:52 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,27 @@
+var menu_list_array = ["Veg pizza","panner pizza","pepperoni pizza","chiken pizza"];
+
+function getmenu(){
+var htmldata;
+menu_list_array.sort();
+        for(var i=0;i<menu_list_array.length;i++){
+            htmldata=htmldata+ menu_list_array[i] + '<br>'
+        }
+        document.getElementById("display_menu").innerHTML = htmldata;
+      }
+
+function add_item(){
+var htmldata;
+var item=document.getElementById("add_item").value;
+menu_list_array.sort();
+        htmldata=""
+        for(var i=0;i<menu_list_array.length;i++){
+            htmldata=htmldata+imgtags+ menu_list_array[i]+'<br>';
+        }
+         document.getElementById("display_addedmenu").innerHTML = htmldata;		
+      }
+
+function add_top(){
+    var item=document.getElementById("add_item").value;
+    menu_list_array.push(item);
+	add_item();
+}
